93b381e150a2e1ee44121a858d84926b2f762064
Innerlight2111 <innerlight2111@gmail.com>
2023-06-09 21:10:39 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'kasm-workplaces',
+    description: 'The househill custom workplace registry.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/Innerlight2111/kasm-workplaces/',
+    contactUrl: 'https://github.com/Innerlight2111/kasm-workplaces/support',
   },
   reactStrictMode: true,
   swcMinify: true,
